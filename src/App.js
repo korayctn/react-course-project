fc39637c92aad0de419c5bdaa3f1a866f923b2a9
koraycetin <korayctnx06@gmail.com>
2023-01-17 23:59:27 +0300

    bulma integration and final state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import "./App.css";
+import "bulma/css/bulma.css";
 import Course from "./Course";
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import CCSharp from "./images/ccsharp.png";
+import Kompleweb from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Course
-        title="Angular"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
-      />
-      <Course
-        title="React"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
-      />
-      <Course
-        title="Bootstrap"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
-      />
-      <Course
-        title="Vuejs"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
-      />
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Courses</p>
+        </div>
+      </section>
+      <section className="section"> </section>
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <Course
+              title="Angular"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
+              img={Angular}
+            />
+          </div>
+          <div className="column">
+            <Course
+              title="React"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
+              img={Bootstrap}
+            />
+          </div>
+          <div className="column">
+            <Course
+              title="Bootstrap"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
+              img={CCSharp}
+            />
+          </div>
+          <div className="column">
+            {" "}
+            <Course
+              title="Vuejs"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, officia numquam. Perspiciatis dignissimos laboriosam, sint incidunt suscipit distinctio nulla dolorum?"
+              img={Kompleweb}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-function Course(props) {
-  const { title, description } = props;
-
+function Course({ title, description, img }) {
   return (
     <>
-      <div>{title}</div>
-      <div>{description}</div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={img} alt="Placeholder image" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            {description}
+            <br />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
